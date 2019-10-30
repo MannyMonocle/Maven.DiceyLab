1338dda71893c3e9eb16096bceb417480ff41758
MannyMonocle <eetumadu@gmail.com>
2019-10-29 20:27:06 -0400

    DiceTest class created

--- src/Test/java/DiceTest.java
+++ src/Test/java/DiceTest.java
@@ -1,11 +1,32 @@
-package java;
-
 import org.junit.Test;
+
 
 public class DiceTest {
 
     @Test
     public void tossAndSumTest(){
+        for(int i = 0; i < 50; i++) {
+            Dice testingDice = new Dice(1);
+            int rollTest = testingDice.tossAndSum();
+            System.out.println(rollTest);
+        }
+    }
 
+    @Test
+    public void tossAndSumTest2dice(){
+        for(int i = 0; i < 50; i++) {
+            Dice testingDice = new Dice(2);
+            int rollTest = testingDice.tossAndSum();
+            System.out.println(rollTest);
+        }
+    }
+
+    @Test
+    public void tossAndSumTest3dice(){
+        for(int i = 0; i < 500; i++) {
+            Dice testingDice = new Dice(3);
+            int rollTest = testingDice.tossAndSum();
+            System.out.println(rollTest);
+        }
     }
 }
