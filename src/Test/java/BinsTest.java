7fef6457856ac8feebb78e62291581896f3b6695
MannyMonocle <eetumadu@gmail.com>
2019-10-30 18:42:57 -0400

    string formatting

--- src/Test/java/BinsTest.java
+++ src/Test/java/BinsTest.java
@@ -1,7 +1,38 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class BinsTest {
 
     @Test
-    
+    public void incrementAndGetOnceTest(){
+        Bins testBin = new Bins(3,8);
+            testBin.incrementBin(7);
+            int actual = testBin.getBin(7);
+
+            Assert.assertEquals(1, actual);
+    }
+
+    @Test
+    public void incrementTwiceAndGetTest(){
+        Bins testBin = new Bins(3,8);
+        testBin.incrementBin(5);
+        testBin.incrementBin(4);
+        testBin.incrementBin(5);
+        int actual = testBin.getBin(5);
+
+        Assert.assertEquals(2, actual);
+    }
+
+    @Test
+    public void incrementTwiceAndPrintTest() {
+        Bins testBin = new Bins(3, 8);
+        testBin.incrementBin(5);
+        testBin.incrementBin(4);
+        testBin.incrementBin(5);
+
+        System.out.println(Arrays.toString(testBin.bin));
+    }
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,40 @@
 public class Simulation {
 
+    int numberOfTosses;
+    int numberOfDies;
+    Dice simDice;
+    Bins results;
 
+    public Simulation(int numberOfDies, int numberOfTosses){
+
+        this.numberOfDies = numberOfDies;
+        this.numberOfTosses = numberOfTosses;
+        this.simDice = new Dice(numberOfDies);
+        this.results = new Bins(numberOfDies, 6 * numberOfDies);
+
+    }
+
+    public Bins runsimulation(){
+
+        for(int i = 0; i < numberOfTosses; i++){
+            results.incrementBin(simDice.tossAndSum());
+        }
+
+        return results;
+
+    }
+
+    public String printResults(){
+
+        this.runsimulation();
+
+        String ans = "***\n + Simulation of "+numberOfDies+" dice tossed for "+numberOfTosses+ " times.\n + *** \n\n";
+
+        for(int i = numberOfTosses; i <= numberOfTosses * 6; i++){
+            ans += String.format("%3x ");
+        }
+
+        return  ans;
+    }
 
 }
